f1ee31782f43772cd3797c2c0b146d2d8e744025
Alexis Labat <alexislabat@hotmail.com>
2022-03-05 16:15:18 +0000

    Initial UI commit

--- Murdle/Assets/Scripts/Test.cs
+++ Murdle/Assets/Scripts/Test.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
-{
+public class Test : MonoBehaviour  {
     // Start is called before the first frame update
     void Start()
     {
