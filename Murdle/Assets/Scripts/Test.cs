0fd6eca5e88d31780ad4d9c9b3da48dcecb28a81
Alexis Labat <alexislabat@hotmail.com>
2022-03-05 17:31:56 +0000

    Added sprites and panel

--- Murdle/Assets/Scripts/Test.cs
+++ Murdle/Assets/Scripts/Test.cs
@@ -1,6 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Test : MonoBehaviour  {
+public class Test : MonoBehaviour {
+    public Image img;
+
+    public Sprite spr1;
+    public Sprite spr2;
+
+    private int count = 1;
+    public const int Max = 2;
+
+    private void Start() {
+        img.sprite = spr1;
+    }
+
+    private void Update() {
+        count = count > 0 ? count + 1 : count - 1;
+        if (count >= Max) {
+            img.sprite = spr1;
+            count = -1;
+        } else if (count <= -1 * Max) {
+            img.sprite = spr2;
+            count = 1;
+        }
+    }
 }
